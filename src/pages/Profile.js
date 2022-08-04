7821087922bda726f21801b9983f2e4f41054202
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-04 17:19:23 -0300

    Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      infoProfile: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await getUser();
+    this.setState({ infoProfile: response }, () => {
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
+    const { loading, infoProfile } = this.state;
+    const { description, email, image, name } = infoProfile;
     return (
-      <>
+      <div data-testid="page-profile">
         <Header />
-        <div data-testid="page-profile">
-          <h1>Profile</h1>
-        </div>
-      </>
+        {
+          loading ? <Loading />
+            : (
+              <div>
+                <img data-testid="profile-image" src={ image } alt={ name } />
+                <h2>Nome:</h2>
+                <h4>{name}</h4>
+                <h4>{name}</h4>
+                <h3>Email:</h3>
+                <p>{email}</p>
+                <h3>Descrição:</h3>
+                <p>{description}</p>
+                <Link
+                  to="/profile/edit"
+                >
+                  Editar perfil
+                </Link>
+              </div>
+            )
+        }
+      </div>
     );
   }
 }
--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -1,17 +1,152 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { getUser, updateUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class ProfileEdit extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentName: '',
+      email: '',
+      image: '',
+      description: '',
+      loading: false,
+      activeButtonSave: true,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await getUser();
+    const { name, email, image, description } = response;
+    this.setState({
+      currentName: name,
+      email,
+      image,
+      description,
+    }, () => {
+      this.setState({ loading: false });
+    });
+  }
+
+  validateEmail = (email) => {
+    const valid = /\S+@\S+\.\S+/;
+    return valid.test(email);
+  }
+
+  handleChange = ({ target }) => {
+    const { currentName, email, image, description } = this.state;
+    const { name } = target;
+    const { value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const isDisable = currentName.length === 0
+    || email.length === 0
+    || image.length === 0
+    || description === 0
+    || this.validateEmail(email) === false;
+      this.setState({ activeButtonSave: isDisable });
+    });
+  }
+
+  saveEdit = async () => {
+    this.setState({ loading: true });
+    const { history } = this.props;
+    const { currentName, email, image, description } = this.state;
+    const infoEdited = {
+      name: currentName,
+      email,
+      image,
+      description,
+    };
+    await updateUser(infoEdited);
+    this.setState({ loading: false });
+    history.push('/profile');
+  }
+
   render() {
+    const { loading,
+      currentName,
+      email,
+      image,
+      description,
+      activeButtonSave } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-profile-edit">
-          <h1>Profile Edit</h1>
+          {
+            loading ? <Loading />
+              : (
+                <>
+                  <h1>Profile Edit</h1>
+                  <form>
+                    <img src={ image } alt={ currentName } />
+                    <input
+                      type="text"
+                      id="input-image"
+                      data-testid="edit-input-image"
+                      name="image"
+                      value={ image }
+                      onChange={ (event) => this.handleChange(event) }
+                    />
+                    <label htmlFor="input-name">
+                      Nome:
+                      <input
+                        type="text"
+                        id="input-name"
+                        data-testid="edit-input-name"
+                        name="currentName"
+                        value={ currentName }
+                        onChange={ (event) => this.handleChange(event) }
+                      />
+                    </label>
+                    <label htmlFor="input-email">
+                      Email:
+                      <input
+                        type="email"
+                        id="input-email"
+                        data-testid="edit-input-email"
+                        name="email"
+                        value={ email }
+                        onChange={ (event) => this.handleChange(event) }
+                      />
+                    </label>
+                    <label htmlFor="text-description">
+                      Descrição:
+                      <textarea
+                        id="text-description"
+                        data-testid="edit-input-description"
+                        name="description"
+                        value={ description }
+                        onChange={ (event) => this.handleChange(event) }
+                      />
+                    </label>
+                    <button
+                      disabled={ activeButtonSave }
+                      type="button"
+                      data-testid="edit-button-save"
+                      onClick={ this.saveEdit }
+                    >
+                      Salvar
+                    </button>
+                  </form>
+                </>
+              )
+          }
         </div>
       </>
     );
   }
 }
+
+ProfileEdit.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default ProfileEdit;

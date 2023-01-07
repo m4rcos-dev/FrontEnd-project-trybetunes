d54e6924dc87a8ab004737035493ee8bcad424a2
m4rcos-dev <dev.marcospereira@gmail.com>
2023-01-06 23:36:34 -0300

    :sparkles: feat: configura pagina home com headers e backgrounds

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import HeaderHorizontal from '../components/HeaderHorizontal';
+import HeaderPages from '../components/HeaderPages';
+import ThemeContext from '../context/ThemeContext';
 
 class Home extends Component {
   render() {
+    const { secondaryTheme } = this.context;
     return (
-      <div>Home</div>
+      <div id="home-container" className={ secondaryTheme }>
+        <Header />
+        <HeaderPages />
+        <HeaderHorizontal />
+      </div>
     );
   }
 }
+
+Home.contextType = ThemeContext;
 
 export default Home;
